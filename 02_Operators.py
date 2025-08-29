2628acdf46179f6e032045c94740f0ea7bf5fa87
muazzahafeez-gif <muazzahafeez@gmail.com>
2025-08-29 14:38:48 +0400

    Python_Operators

---
+++ 02_Operators.py
@@ -0,0 +1,27 @@
+print ("Addition")
+print (5+5)
+print ("Subtraction")
+print (10-5)
+print("Multiplication")
+print (5*5)
+print ("Division to get Floating Value")
+print (10/2)
+print("Division to get Whole Value")
+print (10//2)
+print ("Remainder")
+print (10%2)
+print ("Exponent or Power")
+print (2**3)
+
+#Sequence to Resolve Equation
+#PEMDAS
+#Paranthesis Exponents Multiplication Division Addition Subtraction
+#Left to Right
+
+print ((3*2)/2)
+print ((2*4)*8-4/2)
+print (10+15/5-2)
+
+#To convert any command into comments, you need to select the commands and 
+#then press ctrl+backslash(/)
+#To convert comments into commands you need to press ctrl+backslash(/)
